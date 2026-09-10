cee0d3ac443574e3dd043f7ae0c79f0d5776027b
Adi Firmansyah <adif4659@gmail.com>
2026-09-10 15:41:58 +0700

    feat: mengofigurasi plugins & tools frontend
    daftar plugins & tools:
    - tailwindcss
    - tailwind-merge & clsx
    - prettier-plugin-tailwindcss
    
    serta membuat utility function cn menggunakan tailwind-merge & clsx untuk penggabungan nama kelas css pada classname

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { cn } from "./lib/utils";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/about" element={<h1>About</h1>} />
+        <Route
+          path="/"
+          element={<h1 className={cn("text-3xl font-bold")}>Home</h1>}
+        />
+        <Route
+          path="/about"
+          element={<h1 className={cn("text-3xl font-bold")}>About</h1>}
+        />
       </Routes>
     </BrowserRouter>
   );
