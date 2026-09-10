3cfa4b9cc29dc016dfb448f16d7a25d016e5f939
Adi Firmansyah <adif4659@gmail.com>
2026-09-10 15:20:38 +0700

    feat: memasang & mengonfigurasi react-router-dom

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
