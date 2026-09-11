d0013e7919ebdcbc6d5c45d0ab3ea722987cc15d
Adi Firmansyah <adif4659@gmail.com>
2026-09-11 09:41:25 +0700

    feat: menambahkan dukungan untuk react-hook-form dan zod validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useQuery } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import * as z from "zod";
 import { cn } from "./lib/utils";
 import { useCountStore } from "./stores/useCountStore";
 
+// Contoh penggunaan state management Zustand
 function Count() {
   const { count, increment, decrement } = useCountStore();
 
@@ -39,6 +43,46 @@ function Count() {
   );
 }
 
+// Contoh penggunaan react-hook-form & zod validation
+const schema = z.object({
+  title: z.string().min(1, { error: "Wajib diisi" }),
+});
+
+function CreateProductForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) => console.log(data))}
+      className="flex flex-col gap-2"
+    >
+      <div className="flex flex-col gap-1">
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          type="text"
+          {...register("title")}
+          className="border px-2 py-1"
+        />
+        {errors.title?.message && (
+          <p className="text-sm text-red-500">{errors.title?.message}</p>
+        )}
+      </div>
+
+      <button type="submit" className="cursor-pointer border px-2 py-1">
+        Simpan
+      </button>
+    </form>
+  );
+}
+
+// Contoh penggunaan React Query
 async function getProducts() {
   const response = await fetch("https://fakestoreapi.com/products");
   return response.json();
@@ -54,23 +98,19 @@ function Products() {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div className={cn("grid grid-cols-4 gap-4")}>
-      {data.map((product: any) => (
-        <div
-          key={product.id}
-          className={cn(
-            "flex flex-col items-center justify-center gap-2 rounded border p-4",
-          )}
-        >
-          <img
-            src={product.image}
-            alt={product.title}
-            className={cn("h-32 w-32 object-contain")}
-          />
-          <h2 className={cn("text-lg font-bold")}>{product.title}</h2>
-          <p className={cn("text-gray-500")}>${product.price}</p>
-        </div>
-      ))}
+    <div className="flex flex-col gap-6 p-6">
+      <CreateProductForm />
+      <div className="grid grid-cols-4 gap-3">
+        {data?.map((product: any) => (
+          <div key={product.id} className="border p-2">
+            <img src={product.image} alt={product.title} className="size-10" />
+            <p className="line-clamp-1">{product.title}</p>
+            <p>{product.price}</p>
+            <p className="line-clamp-1">{product.description}</p>
+            <p>{product.category}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
@@ -79,10 +119,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<h1 className={cn("text-3xl font-bold")}>Home</h1>}
-        />
+        {/* Contoh penggunaan react-router-dom */}
+        <Route path="/" element={<h1>Home</h1>} />
+        {/* Contoh penggunaan tailwindcss, tailwind-merge, clsx, dan prettier-plugin-tailwindcss */}
         <Route
           path="/about"
           element={<h1 className={cn("text-3xl font-bold")}>About</h1>}
